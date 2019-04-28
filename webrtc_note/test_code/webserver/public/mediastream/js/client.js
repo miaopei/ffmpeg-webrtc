836d776288438815e4a2015c82f2154692fb1ba4
Mr.Miaow <miaopei163@163.com>
2019-04-28 23:54:54 +0800

    add note

--- webrtc_note/test_code/webserver/public/mediastream/js/client.js
+++ webrtc_note/test_code/webserver/public/mediastream/js/client.js
@@ -1,45 +1,42 @@
 'use strict'
 
-var audioInput = document.querySelector('select#audioInput');
+//devices
+var audioSource = document.querySelector('select#audioSource');
 var audioOutput = document.querySelector('select#audioOutput');
-var videoInput = document.querySelector('select#videoInput');
+var videoSource = document.querySelector('select#videoSource');
 
-var filterSelect = document.querySelector('select#filter');
+//filter
+var filtersSelect = document.querySelector('select#filter');
 
-var videoplay = document.querySelector('video#palyer');
 
 var snapshot = document.querySelector('button#snapshot');
 var picture = document.querySelector('canvas#picture');
-picture.width = 320;
-picture.height = 240;
+picture.width = 640;
+picture.height = 480;
 
+var videoplay = document.querySelector('video#player');
 function gotDevices(deviceInfos)
 {
-    deviceInfos.forEach( function(deviceInfo) {
-        console.log(deviceInfo.kind + ": label = "
-                    + deviceInfo.label + ": id = "
-                    + deviceInfo.deviceId + ": groupId = "
-                    + deviceInfo.groupId);
-
+    deviceInfos.forEach( function(deviceinfo) {
         var option = document.createElement('option');
-        option.text = deviceInfo.label;
-        option.value = deviceInfo.deviceId;
+        option.text = deviceinfo.label;
+        option.value = deviceinfo.deviceId;
 
-        if(deviceInfo.kind === 'audioinput') {
-            audioInput.appendChild(option);
-        } else if (deviceInfo.kind === 'audiooutput') {
+        if(deviceinfo.kind === 'audioinput') {
+			      audioSource.appendChild(option);
+        } else if (deviceinfo.kind === 'audiooutput') {
             audioOutput.appendChild(option);
-        } else if (deviceInfo.kind === 'videoinput') {
-            videoInput.appendChild(option);
+        } else if (deviceinfo.kind === 'videoinput') {
+			      videoSource.appendChild(option);
         }
-    });
+    })
 }
 
 function gotMediaStream(stream)
 {
     //console.log('onRemoteStream stream', stream);
     videoplay.srcObject = stream;
-    return navigator.mediaDevices.enumrateDevices();
+	return navigator.mediaDevices.enumerateDevices();
 }
 
 function handleError(err)
@@ -47,45 +44,45 @@ function handleError(err)
     console.log('getUserMedia error:', err.name);
 }
 
-function start()
+function start() 
 {
-    if(!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia) {
-        console.log('enumerateDevices is not supported!');
-        return;
-    } else {
-        var deviceId = videoInput.value;
-        var constrants = {
-            video: {
-                width: 640,
-                height: 480,
-                frameRate: 30,
-                deviceId: deviceId ? deviceId : undefined
-            },
-            audio: {
+	if(!navigator.mediaDevices ||
+		!navigator.mediaDevices.getUserMedia){
+		console.log('getUserMedia is not supported!');
+		return;
+	} else {
+		var deviceId = videoSource.value; 
+		var constraints = {
+			video : {
+				width: 640,	
+				height: 480,
+				frameRate:15,
+				facingMode: 'enviroment',
+				deviceId : deviceId ? {exact:deviceId} : undefined 
+			}, 
+            audio : {
                 noiseSuppression: true,
                 echoCancellation: true
-            },
-        };
+            } 
+		}
 
-        navigator.mediaDevices.getUserMedia(constrants)
-            .then(gotMediaStream)
-            .then(gotDevices)
-            .catch(handleError);
-    }
+		navigator.mediaDevices.getUserMedia(constraints)
+			.then(gotMediaStream)
+			.then(gotDevices)
+			.catch(handleError);
+	}
 }
 
 start();
 
-videoInput.onchange = start;
+videoSource.onchange = start;
 
-filterSelect.onchange = function() {
-    videoplay.className = filterSelect.value;
+filtersSelect.onchange = function(){
+	videoplay.className = filtersSelect.value;
 }
 
 snapshot.onclick = function() {
-    picture.getContext('2d').drawImage(videoplay, 
-                                       0, 0,
-                                       picture.width,
-                                       picture.height)
+	picture.className = filtersSelect.value;
+	picture.getContext('2d').drawImage(videoplay, 0, 0, picture.width, picture.height);
 }
 
